99cfdc3a19204e3afa7711174bd6cfa45baad111
Sheaxer <sheaxer@gmail.com>
2020-04-09 15:22:49 +0200

    fixed serializing of modification date

--- src/main/java/stuba/fei/gono/java/mongo/json/OffsetDateTimeSerializer.java
+++ src/main/java/stuba/fei/gono/java/mongo/json/OffsetDateTimeSerializer.java
@@ -32,15 +32,16 @@ public class OffsetDateTimeSerializer extends StdSerializer<OffsetDateTime> {
 
         if(tmp.lastIndexOf('Z') != -1)
         {
-            tmp = tmp.substring(0,tmp.lastIndexOf('.')).concat("+00:00");
+            tmp = tmp.substring(0,tmp.lastIndexOf('Z')).concat("+00:00");
         }
-        else {
-            int index1 = tmp.lastIndexOf('.');
-            int index2 = tmp.lastIndexOf('+');
-            if (index2 == -1)
-                index2 = tmp.lastIndexOf('-');
-            tmp = tmp.substring(0, index1).concat(
-                    tmp.substring(index2));
+
+        int index1=tmp.lastIndexOf('.');
+        if(index1!= -1)
+        {
+            int index2= tmp.lastIndexOf('+');
+            if(index2 == -1)
+                index2=tmp.lastIndexOf('-');
+            tmp = tmp.substring(0,index1).concat(tmp.substring(index2));
         }
         log.info(tmp);
         jsonGenerator.writeString(tmp);
